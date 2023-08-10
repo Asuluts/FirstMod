da376293754aa1d19390e84d0ae836f35734f76a
Anthony Spark <anthony.spark1121@gmail.com>
2023-08-09 20:03:36 -0400

    Adding of first item raw tungsten

--- src/main/java/net/jakey/firstmod/TutorialMod.java
+++ src/main/java/net/jakey/firstmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.jakey.firstmodj1;
 
 import com.mojang.logging.LogUtils;
+import net.jakey.firstmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,6 +31,8 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -47,7 +51,10 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.RAWTUNGSTEN);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
